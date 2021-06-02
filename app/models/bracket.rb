26e1c56ba4427e6b486cd9352a511929187e588b
WilsonReese <reese@chicagobooth.edu>
2021-06-02 03:48:16 +0000

    assigning first round matchups works

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -15,7 +15,7 @@ class Bracket < ApplicationRecord
   # make a validation that the number of entries equals an acceptable bracket (8, for now; add 4 and 16 later; add power of 2 later), if not then the bracket needs to fail to create
 
   enum status: { created: "created", in_progress: "in_progress", completed: "completed" }
-  # enum number_of_entries: { eight: 8 } # gonna leave this in to come back to later on
+  # enum number_of_entries: { eight: 8 } # gonna leave this in to come back to later on, might not need it/reformat it
 
   # private
     def create_entries
@@ -30,6 +30,23 @@ class Bracket < ApplicationRecord
         Matchup.create!(bracket: self, position: i + 1)
       end
     end
+
+    def assign_matchups
+      first_round_count = (self.number_of_entries) / 2
+      entries = self.entries
+      matchups = self.matchups
+
+      first_round_count.times do |i|
+        matchup = matchups.find_by(position: i + 1)
+        entry_a = entries.find_by(seed: i + 1)
+        entry_b = entries.find_by(seed: self.number_of_entries - i)
+        matchup.update!(option_a: entry_a, option_b: entry_b)
+      end
+    end
+
+      # matchup = matchups.find_by(position: 1)
+      # entry_a = entries.find_by(seed: 1)
+      # entry_b = entries.find_by(seed: 8)
 
     # def create_reports_for_bench_users
     #   User.on_bench.each do |user|
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -3,14 +3,15 @@ task sample_data: :environment do
   starting = Time.now
 
   if Rails.env.development?
-    Entry.delete_all
     Matchup.delete_all
+    Entry.delete_all
     Bracket.delete_all
   end
 
   b = Bracket.create(number_of_entries: 8)
   b.create_entries
   b.create_matchups
+  b.assign_matchups
 
   ending = Time.now
   p "It took #{(ending - starting).to_i} seconds to create sample data."
